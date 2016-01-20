b6bb5dd23e43f5292d98c2ab96bdd41db510c58e
Gautam <gautam0826@gmail.com>
2016-01-20 12:39:02 -0800

    Update IntlPlayersWiki.java

--- IntlPlayersWiki.java
+++ IntlPlayersWiki.java
@@ -30,10 +30,10 @@ public class IntlPlayersWiki
     
     public static void main(String[] args) throws IOException, ParseException
     {
-        writer = new FileWriter("ForeignPlayersAPCS.csv");
+        writer = new FileWriter("IntlPlayers.csv");
         writer.write("Season,Date,Player,Position,Club From,Club To,Transfer Mode,"
             + "Nationality,Intl Caps,Wikipedia Page Words,League From");
-        for (season = 2012; season < 2016; season++)
+        for (season = 2009; season < 2016; season++)
         {
             scrapeData(season);
         }
